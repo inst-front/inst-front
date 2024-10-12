d11aef16b381d8fa6202a504ab281928f479347e
inst-front <s4shka.5@yandex.by>
2024-10-12 11:03:33 -0700

    Create preview.ts

---
+++ .storybook/preview.ts
@@ -0,0 +1,33 @@
+import type { Preview } from '@storybook/react'
+
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+
+import '../src/styles/index.scss'
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    backgrounds: {
+      default: 'Dark',
+      values: [
+        {
+          name: 'Dark',
+          value: '#000',
+        },
+        {
+          name: 'Light',
+          value: '#f4f4f4',
+        },
+      ],
+    },
+  },
+}
+
+export default preview
